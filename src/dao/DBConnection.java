824a088b2cd806d7e7d201428b684da4b00be843
thom033 <Thomis.rasolomandimby@gmail.com>
2025-01-10 18:14:23 +0300

    Using Connection Pool

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -1,28 +1,52 @@
 package dao;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
+    private static final Properties properties = new Properties();
+    private static final HikariConfig config = new HikariConfig();
+    private static HikariDataSource ds;
 
-    private static final String postgres_hote = "localhost";
-    private static final String postgres_port = "5432"; // Le port par défaut de PostgreSQL est 5432
-    private static final String postgres_bdd = "boulangerie";
-    private static final String postgres_utilisateur = "postgres";
-    private static final String postgres_mdp = "postgres";
-    // private static String postgres_encoding = "WIN1256";
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Failed to load PostgreSQL driver", e);
+        }
+        
+        // Spécifiez le chemin absolu ou relatif du fichier db.properties
+        String propertiesPath = "e:/S5/Boulangerie-Baovola/src/main/ressources/db.properties";
 
-    // private static final String postgresql_url = "jdbc:postgresql://" + postgres_hote + ":" + postgres_port + "/" + postgres_bdd + "?charSet=" + postgres_encoding;
-    private static final String postgresql_url = "jdbc:postgresql://" + postgres_hote + ":" + postgres_port + "/" + postgres_bdd;
+        try (InputStream input = new FileInputStream(propertiesPath)) {
+            properties.load(input);
+        } catch (IOException ex) {
+            throw new RuntimeException("Failed to load db.properties", ex);
+        }
 
-    public static final String postgres_driver = "org.postgresql.Driver";
+        String postgres_hote = properties.getProperty("postgres_hote");
+        String postgres_port = properties.getProperty("postgres_port");
+        String postgres_bdd = properties.getProperty("postgres_bdd");
+        String postgres_utilisateur = properties.getProperty("postgres_utilisateur");
+        String postgres_mdp = properties.getProperty("postgres_mdp");
+        String postgresql_url = "jdbc:postgresql://" + postgres_hote + ":" + postgres_port + "/" + postgres_bdd;
 
-    public static Connection getPostgesConnection() throws ClassNotFoundException, SQLException {
-        Connection postgres = null;
-        Class.forName(postgres_driver); // Utilisez la classe du pilote PostgreSQL
-        postgres = DriverManager.getConnection(postgresql_url, postgres_utilisateur, postgres_mdp);
-        return postgres;
+        config.setJdbcUrl(postgresql_url);
+        config.setUsername(postgres_utilisateur);
+        config.setPassword(postgres_mdp);
+        config.addDataSourceProperty("cachePrepStmts", properties.getProperty("cachePrepStmts"));
+        // Ajoutez d'autres propriétés de configuration si nécessaire
+
+        ds = new HikariDataSource(config);
     }
 
+    public static Connection getPostgesConnection() throws SQLException {
+        return ds.getConnection();
+    }
 }
